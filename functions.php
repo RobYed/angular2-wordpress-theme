a24e1503824eaa4e95cce97fd5c3c9c8ff700a5a
Robert Dey <robert.dey@mandrillmedia.de>
2015-12-28 14:47:17 +0100

    fixed indentation

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 
 function add_scripts() {
-	wp_enqueue_style( 'app-style', get_stylesheet_directory_uri() . '/style.css', '0.1.0', false );
+    wp_enqueue_style( 'app-style', get_stylesheet_directory_uri() . '/style.css', '0.1.0', false );
     wp_enqueue_script( 'app-bundle', get_stylesheet_directory_uri() . '/app.js', '0.1.0', true ); 
 }
 add_action( 'wp_enqueue_scripts', 'add_scripts' );
